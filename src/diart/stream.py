960cfcf06eed307b3030240e0e56a020124c8fcf
Juan Manuel Coria <juanmc2005@hotmail.com>
2022-05-08 19:07:14 +0200

    Rename demo.py to stream.py. Unify script argument docs. Update README.md accordingly

--- src/diart/stream.py
+++ src/diart/stream.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import rx.operators as ops
 import torch
 
+import diart.argdoc as argdoc
 import diart.operators as dops
 import diart.sources as src
 from diart.pipelines import OnlineSpeakerDiarization, PipelineConfig
@@ -12,21 +13,17 @@ from diart.sinks import RealTimePlot, RTTMWriter
 # Define script arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("source", type=str, help="Path to an audio file | 'microphone'")
-parser.add_argument("--step", default=0.5, type=float, help="Source sliding window step")
-parser.add_argument("--latency", default=0.5, type=float, help="System latency")
-parser.add_argument("--tau", default=0.5, type=float, help="Activity threshold tau active")
-parser.add_argument("--rho", default=0.3, type=float, help="Speech duration threshold rho update")
-parser.add_argument("--delta", default=1, type=float, help="Maximum distance threshold delta new")
-parser.add_argument("--gamma", default=3, type=float, help="Parameter gamma for overlapped speech penalty")
-parser.add_argument("--beta", default=10, type=float, help="Parameter beta for overlapped speech penalty")
-parser.add_argument("--max-speakers", default=20, type=int, help="Maximum number of identifiable speakers")
+parser.add_argument("--step", default=0.5, type=float, help=f"{argdoc.STEP}. Defaults to 0.5")
+parser.add_argument("--latency", default=0.5, type=float, help=f"{argdoc.LATENCY}. Defaults to 0.5")
+parser.add_argument("--tau", default=0.5, type=float, help=f"{argdoc.TAU}. Defaults to 0.5")
+parser.add_argument("--rho", default=0.3, type=float, help=f"{argdoc.RHO}. Defaults to 0.3")
+parser.add_argument("--delta", default=1, type=float, help=f"{argdoc.DELTA}. Defaults to 1")
+parser.add_argument("--gamma", default=3, type=float, help=f"{argdoc.GAMMA}. Defaults to 3")
+parser.add_argument("--beta", default=10, type=float, help=f"{argdoc.BETA}. Defaults to 10")
+parser.add_argument("--max-speakers", default=20, type=int, help=f"{argdoc.MAX_SPEAKERS}. Defaults to 20")
 parser.add_argument("--no-plot", dest="no_plot", action="store_true", help="Skip plotting for faster inference")
-parser.add_argument("--gpu", dest="gpu", action="store_true", help="Add this flag to run on GPU")
-parser.add_argument(
-    "--output", type=str,
-    help="Output directory to store the RTTM. Defaults to home directory "
-         "if source is microphone or parent directory if source is a file"
-)
+parser.add_argument("--gpu", dest="gpu", action="store_true", help=argdoc.GPU)
+parser.add_argument("--output", type=str, help=f"{argdoc.OUTPUT}. Defaults to home directory if SOURCE == 'microphone' or parent directory if SOURCE is a file")
 args = parser.parse_args()
 
 # Define online speaker diarization pipeline
