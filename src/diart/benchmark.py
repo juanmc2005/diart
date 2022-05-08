aa8de083cb219fdd8c9a7ee7cbae7d44c2cf68df
Juan Manuel Coria <juanmc2005@hotmail.com>
2022-05-08 19:07:14 +0200

    Rename demo.py to stream.py. Unify script argument docs. Update README.md accordingly

--- src/diart/benchmark.py
+++ src/diart/benchmark.py
@@ -5,6 +5,7 @@ import torch
 from pyannote.database.util import load_rttm
 from pyannote.metrics.diarization import DiarizationErrorRate
 
+import diart.argdoc as argdoc
 import diart.operators as dops
 import diart.sources as src
 from diart.pipelines import OnlineSpeakerDiarization, PipelineConfig
@@ -12,19 +13,19 @@ from diart.sinks import RTTMWriter
 
 # Define script arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("root", type=str, help="Directory with audio files <conversation>.(wav|flac|m4a|...)")
-parser.add_argument("--reference", type=str, help="Directory with RTTM files <conversation>.rttm")
-parser.add_argument("--step", default=0.5, type=float, help="Source sliding window step in seconds. Defaults to 0.5")
-parser.add_argument("--latency", default=0.5, type=float, help="System latency in seconds. Defaults to 0.5")
-parser.add_argument("--tau", default=0.5, type=float, help="Activity threshold tau active in [0,1]. Defaults to 0.5")
-parser.add_argument("--rho", default=0.3, type=float, help="Speech ratio threshold rho update in [0,1]. Defaults to 0.3")
-parser.add_argument("--delta", default=1, type=float, help="Maximum distance threshold delta new in [0,2]. Defaults to 1")
-parser.add_argument("--gamma", default=3, type=float, help="Parameter gamma for overlapped speech penalty. Defaults to 3")
-parser.add_argument("--beta", default=10, type=float, help="Parameter beta for overlapped speech penalty. Defaults to 10")
-parser.add_argument("--max-speakers", default=20, type=int, help="Maximum number of identifiable speakers. Defaults to 20")
-parser.add_argument("--batch-size", default=32, type=int, help="For segmentation and embedding pre-calculation. If lower than 2, run fully online and estimate real-time latency. Defaults to 32")
-parser.add_argument("--output", type=str, help="Output directory to store RTTM files. Defaults to `root`")
-parser.add_argument("--gpu", dest="gpu", action="store_true", help="Add this flag to run on GPU")
+parser.add_argument("root", type=str, help="Directory with audio files CONVERSATION.(wav|flac|m4a|...)")
+parser.add_argument("--reference", type=str, help="Optional. Directory with RTTM files CONVERSATION.rttm. Names must match audio files")
+parser.add_argument("--step", default=0.5, type=float, help=f"{argdoc.STEP}. Defaults to 0.5")
+parser.add_argument("--latency", default=0.5, type=float, help=f"{argdoc.LATENCY}. Defaults to 0.5")
+parser.add_argument("--tau", default=0.5, type=float, help=f"{argdoc.TAU}. Defaults to 0.5")
+parser.add_argument("--rho", default=0.3, type=float, help=f"{argdoc.RHO}. Defaults to 0.3")
+parser.add_argument("--delta", default=1, type=float, help=f"{argdoc.DELTA}. Defaults to 1")
+parser.add_argument("--gamma", default=3, type=float, help=f"{argdoc.GAMMA}. Defaults to 3")
+parser.add_argument("--beta", default=10, type=float, help=f"{argdoc.BETA}. Defaults to 10")
+parser.add_argument("--max-speakers", default=20, type=int, help=f"{argdoc.MAX_SPEAKERS}. Defaults to 20")
+parser.add_argument("--batch-size", default=32, type=int, help="For segmentation and embedding pre-calculation. If BATCH_SIZE < 2, run fully online and estimate real-time latency. Defaults to 32")
+parser.add_argument("--gpu", dest="gpu", action="store_true", help=argdoc.GPU)
+parser.add_argument("--output", type=str, help=f"{argdoc.OUTPUT}. Defaults to `root`")
 args = parser.parse_args()
 
 args.root = Path(args.root)
