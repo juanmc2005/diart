db3fd9dc3df9584822269acd962f4d376e5330a8
Juan Manuel Coria <juanmc2005@hotmail.com>
2022-05-13 10:01:50 +0200

    Add docstrings to RealTimeInference and Benchmark

--- src/diart/inference.py
+++ src/diart/inference.py
@@ -3,6 +3,7 @@ from typing import Union, Text, Optional
 
 import pandas as pd
 import rx.operators as ops
+from pyannote.core import Annotation
 from pyannote.database.util import load_rttm
 from pyannote.metrics.diarization import DiarizationErrorRate
 
@@ -13,13 +14,40 @@ from diart.sinks import RTTMWriter, RealTimePlot
 
 
 class RealTimeInference:
+    """
+    Streams an audio source to an online speaker diarization pipeline.
+    It writes predictions to an output directory in RTTM format, and also plot predictions in real time.
+
+    Parameters
+    ----------
+    output_path: Text or Path
+        Output directory to store predictions in RTTM format.
+    do_plot: bool
+        Whether to draw predictions in a moving plot. Defaults to True.
+    """
     def __init__(self, output_path: Union[Text, Path], do_plot: bool = True):
         self.output_path = Path(output_path).expanduser()
         self.output_path.mkdir(parents=True, exist_ok=True)
         self.do_plot = do_plot
 
-    def __call__(self, pipeline: OnlineSpeakerDiarization, source: src.AudioSource):
-        rttm_writer = RTTMWriter(path=self.output_path / f"{source.uri}.rttm")
+    def __call__(self, pipeline: OnlineSpeakerDiarization, source: src.AudioSource) -> Annotation:
+        """
+        Stream audio chunks from `source` to `pipeline` and write predictions to disk.
+
+        Parameters
+        ----------
+        pipeline: OnlineSpeakerDiarization
+            Configured speaker diarization pipeline.
+        source: AudioSource
+            Audio source to be read and streamed.
+
+        Returns
+        -------
+        predictions: Annotation
+            Speaker diarization pipeline predictions
+        """
+        rttm_path = self.output_path / f"{source.uri}.rttm"
+        rttm_writer = RTTMWriter(path=rttm_path)
         observable = pipeline.from_source(source).pipe(
             dops.progress(f"Streaming {source.uri}", total=source.length, leave=True)
         )
@@ -40,8 +68,25 @@ class RealTimeInference:
         # Stream audio through the pipeline
         source.read()
 
+        return load_rttm(rttm_path)[source.uri]
+
 
 class Benchmark:
+    """
+    Run an online speaker diarization pipeline on a set of audio files in batches.
+    It writes predictions to a given output directory.
+
+    If the reference is given, it calculates the average diarization error rate.
+
+    Parameters
+    ----------
+    speech_path: Text or Path
+        Directory with audio files.
+    reference_path: Text or Path
+        Directory with reference RTTM files (same names as audio files).
+    output_path: Text or Path
+        Output directory to store predictions in RTTM format.
+    """
     def __init__(
         self,
         speech_path: Union[Text, Path],
@@ -63,7 +108,23 @@ class Benchmark:
             self.output_path.mkdir(parents=True, exist_ok=True)
 
     def __call__(self, pipeline: OnlineSpeakerDiarization, batch_size: int = 32) -> Optional[pd.DataFrame]:
-        # Run inference
+        """
+        Run a given pipeline on a set of audio files using
+        pre-calculated segmentation and embeddings in batches.
+
+        Parameters
+        ----------
+        pipeline: OnlineSpeakerDiarization
+            Configured speaker diarization pipeline.
+        batch_size: int
+            Batch size. Defaults to 32.
+
+        Returns
+        -------
+        performance: pandas.DataFrame, optional
+            DataFrame with detailed performance on each file, as well as average performance.
+            None if the reference is not provided.
+        """
         chunk_loader = src.ChunkLoader(pipeline.sample_rate, pipeline.duration, pipeline.config.step)
         for filepath in self.speech_path.iterdir():
             num_chunks = chunk_loader.num_chunks(filepath)
