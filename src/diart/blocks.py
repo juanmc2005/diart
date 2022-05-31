7f771731b5cb9e99f80bf933d116d2137f5f72df
Juan Manuel Coria <juanmc2005@hotmail.com>
2022-06-01 00:48:54 +0200

    Make Binarize more efficient by looking at all speakers at the same time. Add docstring to Binarize

--- src/diart/blocks.py
+++ src/diart/blocks.py
@@ -1,4 +1,4 @@
-from typing import Union, Optional, List, Iterable, Tuple
+from typing import Union, Optional, List, Iterable, Tuple, Text
 
 import numpy as np
 import torch
@@ -560,25 +560,51 @@ class OnlineSpeakerClustering:
 
 
 class Binarize:
-    def __init__(self, uri: str, threshold: float):
+    """
+    Transform a speaker segmentation from the discrete-time domain
+    into a continuous-time speaker segmentation.
+
+    Parameters
+    ----------
+    uri: Text
+        Uri of the audio stream.
+    threshold: float
+        Probability threshold to determine if a speaker is active at a given frame.
+    """
+
+    def __init__(self, uri: Text, threshold: float):
         self.uri = uri
         self.threshold = threshold
 
     def __call__(self, segmentation: SlidingWindowFeature) -> Annotation:
+        """
+        Return the continuous-time segmentation
+        corresponding to the discrete-time input segmentation.
+
+        Parameters
+        ----------
+        segmentation: SlidingWindowFeature
+            Discrete-time speaker segmentation.
+
+        Returns
+        -------
+        annotation: Annotation
+            Continuous-time speaker segmentation.
+        """
         num_frames, num_speakers = segmentation.data.shape
         timestamps = segmentation.sliding_window
-        is_active = segmentation.data.T > self.threshold  # shape (speakers, frames)
+        is_active = segmentation.data > self.threshold
         # Artificially add last inactive frame to close any remaining speaker turns
-        is_active = np.append(is_active, [[False]] * num_speakers, axis=1)
+        is_active = np.append(is_active, [[False] * num_speakers], axis=0)
+        start_times = np.zeros(num_speakers) + timestamps[0].middle
         annotation = Annotation(uri=self.uri, modality="speech")
-        for spk in range(num_speakers):
-            start = timestamps[0].middle
-            for t in range(num_frames):
-                # Any (False, True) start a speaker turn at "True" index
-                if not is_active[spk, t] and is_active[spk, t + 1]:
-                    start = timestamps[t + 1].middle
-                # Any (True, False) end a speaker turn at "False" index
-                elif is_active[spk, t] and not is_active[spk, t + 1]:
-                    region = Segment(start, timestamps[t + 1].middle)
-                    annotation[region, spk] = f"speaker{spk}"
+        for t in range(num_frames):
+            # Any (False, True) starts a speaker turn at "True" index
+            onsets = np.logical_and(np.logical_not(is_active[t]), is_active[t + 1])
+            start_times[onsets] = timestamps[t + 1].middle
+            # Any (True, False) ends a speaker turn at "False" index
+            offsets = np.logical_and(is_active[t], np.logical_not(is_active[t + 1]))
+            for spk in np.where(offsets)[0]:
+                region = Segment(start_times[spk], timestamps[t + 1].middle)
+                annotation[region, spk] = f"speaker{spk}"
         return annotation
