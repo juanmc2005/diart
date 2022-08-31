e4a11b06115104ee3888df13919c4ec8f8fc585f
Juan Manuel Coria <juanmc2005@hotmail.com>
2022-08-31 12:48:43 +0200

    Export csv report in diart.benchmark when output is provided

--- src/diart/benchmark.py
+++ src/diart/benchmark.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 
 import torch
 
@@ -10,12 +11,12 @@ from diart.pipelines import OnlineSpeakerDiarization, PipelineConfig
 
 def run():
     parser = argparse.ArgumentParser()
-    parser.add_argument("root", type=str, help="Directory with audio files CONVERSATION.(wav|flac|m4a|...)")
+    parser.add_argument("root", type=Path, help="Directory with audio files CONVERSATION.(wav|flac|m4a|...)")
     parser.add_argument("--segmentation", default="pyannote/segmentation", type=str,
                         help=f"{argdoc.SEGMENTATION}. Defaults to pyannote/segmentation")
     parser.add_argument("--embedding", default="pyannote/embedding", type=str,
                         help=f"{argdoc.EMBEDDING}. Defaults to pyannote/embedding")
-    parser.add_argument("--reference", type=str,
+    parser.add_argument("--reference", type=Path,
                         help="Optional. Directory with RTTM files CONVERSATION.rttm. Names must match audio files")
     parser.add_argument("--step", default=0.5, type=float, help=f"{argdoc.STEP}. Defaults to 0.5")
     parser.add_argument("--latency", default=0.5, type=float, help=f"{argdoc.LATENCY}. Defaults to 0.5")
@@ -28,7 +29,7 @@ def run():
     parser.add_argument("--batch-size", default=32, type=int, help=f"{argdoc.BATCH_SIZE}. Defaults to 32")
     parser.add_argument("--cpu", dest="cpu", action="store_true",
                         help=f"{argdoc.CPU}. Defaults to GPU if available, CPU otherwise")
-    parser.add_argument("--output", type=str, help=f"{argdoc.OUTPUT}. Defaults to no writing")
+    parser.add_argument("--output", type=Path, help=f"{argdoc.OUTPUT}. Defaults to no writing")
     args = parser.parse_args()
     args.device = torch.device("cpu") if args.cpu else None
     args.segmentation = SegmentationModel.from_pyannote(args.segmentation)
@@ -43,7 +44,10 @@ def run():
         batch_size=args.batch_size,
     )
 
-    benchmark(OnlineSpeakerDiarization(PipelineConfig.from_namespace(args), profile=True))
+    pipeline = OnlineSpeakerDiarization(PipelineConfig.from_namespace(args), profile=True)
+    report = benchmark(pipeline)
+    if args.output is not None and report is not None:
+        report.to_csv(args.output / "benchmark_report.csv")
 
 
 if __name__ == "__main__":
