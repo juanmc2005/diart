ae92e52642bbbee6ca95e3e7fbac32784d0191ee
juanmc2005 <juanmc2005@hotmail.com>
2022-09-15 13:51:00 +0200

    Add AudioSource.close() to close streams correctly if needed

--- src/diart/sources.py
+++ src/diart/sources.py
@@ -11,6 +11,7 @@ import websockets
 from einops import rearrange
 from rx.subject import Subject
 from torchaudio.io import StreamReader
+import signal
 
 from .audio import FilePath, AudioLoader
 
@@ -32,11 +33,15 @@ class AudioSource:
 
     @property
     def duration(self) -> Optional[float]:
-        """The duration of the stream if known. Defaults to None (unknown duration)"""
+        """The duration of the stream if known. Defaults to None (unknown duration)."""
         return None
 
     def read(self):
-        """Start reading the source and yielding samples through the stream"""
+        """Start reading the source and yielding samples through the stream."""
+        raise NotImplementedError
+
+    def close(self):
+        """Stop reading the source and close all open streams."""
         raise NotImplementedError
 
 
@@ -64,6 +69,7 @@ class FileAudioSource(AudioSource):
         self.resolution = 1 / self.sample_rate
         self.block_size = block_size
         self.padding_end = padding_end
+        self.is_closed = False
 
     @property
     def duration(self) -> Optional[float]:
@@ -97,10 +103,18 @@ class FileAudioSource(AudioSource):
         # Stream blocks
         for i, waveform in enumerate(chunks):
             try:
+                if self.is_closed:
+                    break
                 self.stream.on_next(waveform)
+            except KeyboardInterrupt:
+                break
             except Exception as e:
                 self.stream.on_error(e)
         self.stream.on_completed()
+        self.close()
+
+    def close(self):
+        self.is_closed = True
 
 
 class MicrophoneAudioSource(AudioSource):
@@ -109,27 +123,37 @@ class MicrophoneAudioSource(AudioSource):
     def __init__(self, sample_rate: int, block_size: int = 1000):
         super().__init__("live_recording", sample_rate)
         self.block_size = block_size
-        self.mic_stream = sd.InputStream(
+        self._mic_stream = sd.InputStream(
             channels=1,
             samplerate=sample_rate,
             latency=0,
             blocksize=self.block_size,
             callback=self._read_callback
         )
-        self.queue = SimpleQueue()
+        self._queue = SimpleQueue()
 
     def _read_callback(self, samples, *args):
-        self.queue.put_nowait(samples[:, [0]].T)
+        self._queue.put_nowait(samples[:, [0]].T)
 
     def read(self):
-        self.mic_stream.start()
-        while self.mic_stream:
+        self._mic_stream.start()
+        while self._mic_stream:
             try:
-                self.stream.on_next(self.queue.get())
+                while self._queue.empty():
+                    if self._mic_stream.closed:
+                        break
+                self.stream.on_next(self._queue.get_nowait())
+            except KeyboardInterrupt:
+                break
             except Exception as e:
                 self.stream.on_error(e)
                 break
         self.stream.on_completed()
+        self.close()
+
+    def close(self):
+        self._mic_stream.stop()
+        self._mic_stream.close()
 
 
 class WebSocketAudioSource(AudioSource):
@@ -153,6 +177,7 @@ class WebSocketAudioSource(AudioSource):
         self.host = host
         self.port = port
         self.websocket = None
+        self.stop = None
 
     async def _ws_handler(self, websocket):
         self.websocket = websocket
@@ -165,12 +190,16 @@ class WebSocketAudioSource(AudioSource):
                 # Reshape and send through
                 self.stream.on_next(samples.reshape(1, -1))
             self.stream.on_completed()
+            self.close()
         except websockets.ConnectionClosedError as e:
             self.stream.on_error(e)
 
     async def _async_read(self):
+        loop = asyncio.get_running_loop()
+        self.stop = loop.create_future()
+        loop.add_signal_handler(signal.SIGTERM, self.stop.set_result, None)
         async with websockets.serve(self._ws_handler, self.host, self.port):
-            await asyncio.Future()
+            await self.stop
 
     async def _async_send(self, message: AnyStr):
         await self.websocket.send(message)
@@ -178,6 +207,11 @@ class WebSocketAudioSource(AudioSource):
     def read(self):
         """Starts running the websocket server and listening for audio chunks"""
         asyncio.run(self._async_read())
+
+    def close(self):
+        if self.websocket is not None:
+            # The value could be anything
+            self.stop.set_result(True)
 
     def send(self, message: AnyStr):
         """Send a message through the current websocket.
@@ -221,14 +255,23 @@ class TorchStreamAudioSource(AudioSource):
             format="fltp",
             sample_rate=self.sample_rate,
         )
+        self.is_closed = False
 
     def read(self):
         for item in self._streamer.stream():
             try:
+                if self.is_closed:
+                    break
                 # shape (samples, channels) to (1, samples)
                 chunk = np.mean(item[0].numpy(), axis=1, keepdims=True).T
                 self.stream.on_next(chunk)
+            except KeyboardInterrupt:
+                break
             except Exception as e:
                 self.stream.on_error(e)
                 break
         self.stream.on_completed()
+        self.close()
+
+    def close(self):
+        self.is_closed = True
